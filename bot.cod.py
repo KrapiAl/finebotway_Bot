37b1ff9b0e4f57a9018a77d5bd3f27c5c499f879
KrapiAl <116262176+KrapiAl@users.noreply.github.com>
2022-12-20 19:29:54 +0300

    Update bot.cod.py

--- bot.cod.py
+++ bot.cod.py
@@ -8,6 +8,9 @@ import yfinance as yf
 
 
 class Bot:
+    '''
+    этот класс является просто удобным оформлением для кода бота, в дальнейшем так будет проще совершенствовать его
+    '''
     Token = ""
     bot = 0
     commands = []
@@ -22,10 +25,19 @@ class Bot:
         pass
 
 class FinanceBot(Bot):
+    '''
+    это класс непосредственно работающего бота
+    '''
     bb = 0
     #bot = 0
 
-    def price_check(self, name, n, period): #эта функция создаёт файл с ценами акции (считывание каждые 5 минут каждый час), где "name" это код акции
+    def price_check(self, name, n, period):
+        '''
+        эта функция создаёт файл с ценами акции (считывание каждые 5 минут каждый час), где 
+        name - это тикер акции и название файла
+        n - частота считывания
+        period - период считывания
+        '''
             file_name = str(name + '.txt')
             while 1:
                 #C = [0] * n #n -- это количество считываний за период. Например, 12 (за период час)
@@ -48,7 +60,12 @@ class FinanceBot(Bot):
                         now_aam = now + timedelta(minutes=k)
                         pause.until(now_aam)
 
-    def price_read(self, name, n, j):  # j - это сдвиг для медианной цены
+    def price_read(self, name, n, j):
+        '''
+        эта функция достаёт данные о ценах из соответствующих файлов
+        name - это тикер акции
+        j - это сдвиг во времени(для медианной цены)
+        '''
         C = [0] * n
         file1 = open(name, "r")  # name должно быть в формате "sample.txt"
         for i in range(0, n):
@@ -59,6 +76,10 @@ class FinanceBot(Bot):
         return C
 
     def median_price(self, name):
+        '''
+        эта функция создаёт массив со средними ценами акции за необходимый промежуток времени
+        name - это тикер акции
+        '''
         n = 12
         M = [0] * n
         for i in range(0, n):
@@ -70,19 +91,33 @@ class FinanceBot(Bot):
             M[i] = median
         return M
 
-    def SMMA(self, name, M, n, shift=1):  # сглаженное скользящее среднее
+    def SMMA(self, name, M, n, shift=1):
+        '''
+        эта функция считает сглаженное скользящее среднее
+        name - это тикер акции
+        '''
         if shift == 1:
             smma = sum(M) / n
         else:
             smma = (sum(M) - SMMA(self, name, M, n, shift - 1) + M[0]) / n
         return smma
 
-    def SMA(self, name, n):  # Простое скользящее среднее (Simple Moving Average)
+    def SMA(self, name, n):
+        '''
+        эта функция считает простое скользящее среднее (Simple Moving Average)
+        name - это тикер акции
+        n - требуемый период
+        '''
         C = price_read(self, name, n, 0)
         sma = sum(C) / n
         return sma
 
-    def EMA(self, name, n):  # функця ситает экспоненцальное скользящее среднее
+    def EMA(self, name, n):
+        '''
+        эта функция считает экспоненцальное скользящее среднее
+        name - это тикер акции
+        n - требуемый период
+        '''
         a = 0.02
         if n == 1:
             ema = SMA(self, name, n)
@@ -91,15 +126,26 @@ class FinanceBot(Bot):
             ema = EMA(self, name, n - 1) + a * (price_read(self, name, n, 0)[-1] - EMA(self, name, n - 1))
             return ema
 
-    def MACD(self, name):  # индикатор MACD
+    def MACD(self, name):
+        '''
+        индикатор MACD
+        name - это тикер акции
+        '''
         macd = EMA(name, 12) - EMA(name, 26)
         return macd
 
     def SIG_MACD(self, name):
+        '''
+        сигнальная линия для индикатора MACD
+        name - это тикер акции
+        '''
         sig = SMA(self, name, 9)
         return sig
 
-    def monotonicity(self, name):  # вот с этой функцией больше всего проблем, я не придумала, как отпределить возрастает цена или убывает, чтобы результат был актуалень для периода, а не для поледних секунд
+    def monotonicity(self, name):
+        '''
+        функция определяет монотонность графика
+        '''
         C = price_read(self, name, 5, 0)  # тут нулевой элемент это последнне значение цены
         if C[0] >= C[4]:
             return 1
@@ -107,6 +153,12 @@ class FinanceBot(Bot):
             return 0
 
     def SAR(self, name, period, high, low):
+        '''
+        функция строит пораболик SAR
+        name - это тикер акции
+        period - требуемый период
+        high/low - экстремумы цены за выбранный период
+        '''
         a = 2  # a - фактор ускорения, можно будет потом отдельно высчитывать для более точных результатов
         if period == 0:
             sar = price_read(self, name, 12, 0)[-1]
@@ -118,6 +170,10 @@ class FinanceBot(Bot):
         return sar
 
     def Williams_Alligator(self, name):  # позже нужно сделать более точным
+        '''
+        функция определяет разнесены ли три графика индикатора Williams_Alligator
+        name - это тикер акции
+        '''
         A = [0] * 3
         # mp = (high + low) / 2
         M = median_price(self, name)
@@ -130,6 +186,10 @@ class FinanceBot(Bot):
             return 0
 
     def RSI(self, name):
+                '''
+        функция определяет положение и монотонность индикатора Stochastic RIC
+        name - это тикер акции
+        '''
         period_tipe = 168  # 168 - в лучае 14-ти часового варианта
         n = 14 * period_tipe
         minn14 = min(price_read(self, name, n, 0))
@@ -151,6 +211,10 @@ class FinanceBot(Bot):
             return 0
 
     def buy(self, name):
+        '''
+        это самая фажная функция. Она собирает данные всех индикаторов и определяет стоит ли покупать или продавать акцию
+        ame - это тикер акции
+        '''
         while 1:
             n = 12  # эквивалентно часу
             period = 0
@@ -172,8 +236,8 @@ class FinanceBot(Bot):
                 if SAR(self, name, period, high, low) < current_price:
                     if Williams_Alligator(self, name, high, low) == 1:
                         if RSI(self, name) == 1:
-                            global bb
-                            bb = 1
+                            bot.send_message(message.chat.id, "продажа, такер:')
+                            bot.send_message(message.chat.id, name)
             elif macd_locaton1 != macd_locaton2 and macd_locaton1 > macd_locaton2:
                 low = min(price_read(self, name, n, 0))
                 high = max(price_read(self,name, n, 0))
@@ -181,17 +245,17 @@ class FinanceBot(Bot):
                 if SAR(self, name, period, high, low) > current_price:
                     if Williams_Alligator(self, name, high, low) == 1:
                         if RSI(self, name) == 0:
-                            global bb
-                            bb = 2
+                            bot.send_message(message.chat.id, "продажа, такер:')
+                            bot.send_message(message.chat.id, name)
 
 
     def background_funs(self):
         stocks = ['NLFX', 'MSFT', 'AAPL', 'TSLA', 'AMZN', 'GOOG', 'F', 'INTC', 'UBER'] # F = ford motor company, INTC = intel corporation
         for i in stocks:
-            th = threading.Thread(target=self.price_check(i, 12, 60))
+            th = threading.Thread(target=self.price_check(self, i, 12, 60))
             th.start()
-        #th2 = threading.Thread(target=self.buy(self))
-        #th2.start()
+            th2 = threading.Thread(target=self.buy(self, i, 12, 60))
+            th2.start()
 
     def init(self, token):
         Bot.init(self, token)
@@ -201,7 +265,6 @@ class FinanceBot(Bot):
     @self.bot.message_handler(commands=["/start_m"])
     def start_m(message):
         bot.send_message(message.chat.id, "Добрый день! Теперь этот бот будет присылать вам информацию потенцавльной ползе покупки/продажи акций")
-        #self.bot.send_message(message.chat.id, str(self.count))
 
     def start(self):
         Bot.start(self)
