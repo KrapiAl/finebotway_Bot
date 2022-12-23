d54d922e2626baad77d62c8f38d125e6629726f5
KrapiAl <116262176+KrapiAl@users.noreply.github.com>
2022-12-23 22:53:50 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 import unittest
+import check
 
-
-class TestSum(unittest.TestCase):
-
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
-
-if __name__ == 'main':
-    unittest.main()
-    print("Everything passed")
+class TestBot(unittest.TestCase):
+  #setUp method is overridden from the parent class TestCase
+  def setUp(self):
+    pass
+  #Each test method starts with the keyword test_
+  def test_SMA(self):
+    self.assertEqual(check.SMA('fortest.txt', 10), 34)
+  def test_priceread(self):
+    self.assertEqual(check.price_read('fortest.txt', 3, 0), [5.000, 10.000, 15.000])
+  def test_sig_macd(self):
+    self.assertEqual(check.SIG_MACD('fortest.txt'), 32.22222222222222)
+    '''
+  def test_divide(self):
+    self.assertEqual(self.calculator.divide(10,5), 5)'''
+# Executing the tests in the above test case class
+if __name__ == "__main__":
+  unittest.main()
